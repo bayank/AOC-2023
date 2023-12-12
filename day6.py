3ceebbfaa943d37f997d1979180844cb181f3d85
bayank <bayank@gmail.com>
2023-12-11 19:02:42 -0500

    Day 6, refactored

--- day6.py
+++ day6.py
@@ -14,14 +14,21 @@ def ways_to_win(race_time, record_dist):
         if total_distance > record_dist:
             num_ways_to_win += 1
     return num_ways_to_win
-if __name__ == '__main__':
-    part1 = (
-            (ways_to_win(56,546))
-             * (ways_to_win(97, 1927))
-             * (ways_to_win(78, 1131))
-             * (ways_to_win(75,1139))
-    )
-    print(f'Part 1: {part1}')
 
-    part2 = ways_to_win(56977875,546192711311139)
+
+def join_list_elements(list_elements):
+    return int(''.join(map(str, list_elements)))
+
+
+if __name__ == '__main__':
+    times = [56, 97, 78, 75]
+    record_dists = [546, 1927, 1131, 1139]
+
+    total_ways_to_win = 1
+    for i in range(len(times)):
+        total_ways_to_win *= ways_to_win(times[i], record_dists[i])
+
+    print(f'Part 1: {total_ways_to_win}')
+
+    part2 = ways_to_win(join_list_elements(times),join_list_elements(record_dists))
     print(f'Part 2: {part2}')
